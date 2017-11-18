a797f5b9e15669abbc2157c5cdd80c62f3191cac
cfsanderson <calebsanderson@gmail.com>
2017-11-18 09:38:47 -0500

    Round 2 of 01 drum kit.

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,9 @@
+/* 
+- listen for the keydown event 
+- run a function which will find the audio element and play it
+- add a class of playing so that the CSS works
+
+*/ 
+
+window.addEventListener('keydown', function(e) {
+})
