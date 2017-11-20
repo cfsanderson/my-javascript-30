000ed7555d2e48b211fb491c3ebd72cb2013519f
cfsanderson <calebsanderson@gmail.com>
2017-11-19 22:55:12 -0500

    updates 01-javascript-drum-kit

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -6,4 +6,18 @@
 */ 
 
 window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  // data selector syntax ^
+  if (!audio) return
+  audio.currentTime = 0; /* rewind to the start */ 
+  audio.play()
+  key.classList.add('playing');
 })
+
+function removeTransition(e) {
+  (e.propertyName !== 'transform') ? return : console.log(e.propertyName)
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
